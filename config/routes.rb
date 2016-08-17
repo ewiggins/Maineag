5cc9de88109f748744c626c3ac15127797024945
ewigg <leighw33@gmail.com>
2016-08-17 23:50:20 +0000

    Build out Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :constituents
   resources :events
+ 
+  root 'events#index'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
